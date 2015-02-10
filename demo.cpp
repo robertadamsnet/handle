c1adb43511f0e9313daa506330596fabf8bc856e
Robert Adams <robert@robertadams.net>
2015-02-10 22:25:34 +0000

    Initial commit of handle package.

---
+++ demo.cpp
@@ -0,0 +1,142 @@
+/* demo.hpp
+ * 
+ * Copyright (c) 2015 by Robert T. Adams 
+ * All rights reserved.
+ *
+ * The MIT License (MIT)
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+#include "handle.hpp"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Animal {
+public:
+  string name() const { return v_name(); }
+private:
+  virtual string v_name() const = 0;
+};
+
+class Cat : public Animal {
+public:
+  string meow() {
+    return  "meow";
+  }
+private:
+  virtual string v_name() const override {
+    return "cat";
+  }
+};
+
+class Dog : public Animal {
+public:
+  string woof() {
+    return "woof";
+  }
+private:
+  virtual string v_name() const override {
+    return "dog";
+  }
+};
+
+class Fox : public Animal {
+public:
+ string say() {
+   return "???";
+ }
+private:
+ virtual string v_name() const override {
+   return "fox";
+ }
+};
+
+void dog_service(Dog& dog) {
+  cout << "The dog says: " << dog.woof() << endl;
+}
+
+void cat_service(Cat& cat) {
+  cout << "The cat says: " << cat.meow() << endl;
+  cat.meow();
+}
+
+auto fox_service = [](Fox& fox) {
+    cout << "The fox says: " << fox.say() << endl;
+    fox.say();
+};
+
+class Vet {
+public:
+  void operator()(Fox& fox) {
+    say(fox, fox.say());
+  }
+
+  void operator()(Dog& dog) {
+    say(dog, dog.woof());
+  }
+
+  void operator()(Cat& cat) {
+    say(cat, cat.meow());
+  }
+
+  void say(Animal& animal, string phrase) {
+    string name = animal.name();
+    cout << "Fixing a " << name << "..." << endl
+         << "\"" << phrase << "!\"" << endl
+         << endl;
+  }
+} vet_service;
+
+int main() {
+  Service service;
+
+  service.add<Dog>(dog_service);
+  service.add<Cat>(cat_service);
+  service.add<Fox>(fox_service);
+
+  Dog dog;
+  Cat cat;
+  Fox fox;
+
+  vector<Handle> handles = {
+    Handle(dog),
+    Handle(cat),
+    Handle(fox)
+  };
+
+  for(auto handle : handles) {
+    handle.visit(service);
+  }
+
+  cout << endl;
+
+  service.add<Dog>(vet_service);
+  service.add<Cat>(vet_service);
+  service.add<Fox>(vet_service);
+
+  for(auto animal : handles) {
+    animal.visit(service);
+  }
+
+  return 0;
+}
+
---
+++ handle.hpp
@@ -0,0 +1,89 @@
+/* handle.hpp
+ * 
+ * Copyright (c) 2015 by Robert T. Adams 
+ * All rights reserved.
+ *
+ * The MIT License (MIT)
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+#ifndef handle_hpp_20150209_1149
+#define handle_hpp_20150209_1149
+
+#include <functional>
+#include <typeindex>
+#include <map>
+
+
+
+class Service final {
+public:
+  Service() {
+  }
+  template<class resource_type>
+  void add(std::function<void(resource_type&)> fn) {
+    handler<resource_type>() = fn;
+  }
+
+  template<class resource_type>
+  void operator()(resource_type& resource) {
+    auto handle = handler<resource_type>();
+    handle(resource);
+  }
+private:
+  template<class resource_type>
+  static std::function<void(resource_type&)>& handler() {
+    static std::function<void(resource_type&)> handler_ = 
+      default_handler<resource_type>;
+    return handler_;
+  }
+
+  template<class resource_type>
+  static void default_handler(resource_type&) { }
+};
+
+
+class Handle {
+public:
+
+  template<class resource_type>
+  explicit Handle(resource_type& r) : unknown_(&r) {
+    visit_ = [&](Service& service) {
+      resource_type& resource = *static_cast<resource_type*>(unknown_);
+      service(resource);
+    };
+  }
+
+  void visit(Service& service) {
+    visit_(service);
+  }
+
+  Handle(const Handle& r) : unknown_(r.unknown_), visit_(r.visit_) {
+
+  }
+
+  Handle(Handle&& r) noexcept : unknown_(r.unknown_), visit_(std::move(r.visit_)) {
+  }
+private:
+  void* unknown_;
+  std::function<void(Service&)> visit_;
+};
+#endif//handle_hpp_20150209_1149
