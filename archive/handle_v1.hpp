fc5954ca58b060407f9b0d30ace1be849f580741
Robert Adams <robert@robertadams.net>
2015-02-13 13:31:46 +0000

    Added archive folder to keep older versions of the Handle package.

---
+++ archive/handle_v1.hpp
@@ -0,0 +1,55 @@
+#ifndef handle_hpp_20150209_1149
+#define handle_hpp_20150209_1149
+
+#include <functional>
+#include <typeindex>
+#include <map>
+
+class Server {
+public:
+  template<class resource_type>
+  void add(std::function<void(resource_type&)> fn) {
+    using namespace std;
+    specialty_map.insert(make_pair(type_index(typeid(resource_type)), &fn));
+  }
+
+  template<class resource_type>
+  void service(resource_type& r) {
+    using namespace std;
+    typedef std::function<void(resource_type&)> fn_type;
+    auto found_fn = specialty_map.find(type_index(typeid(resource_type)));
+    if(found_fn != specialty_map.end()) {
+      auto fn = *static_cast<fn_type*>(found_fn->second);
+      fn(r);
+    }
+  }
+private:
+  std::map<std::type_index, void*> specialty_map;
+};
+
+class Handle {
+public:
+  template<class resource_type>
+  explicit Handle(resource_type& r) : unknown_(&r) {
+    visit_ = [&](Server& s) {
+      resource_type& resource = *static_cast<resource_type*>(unknown_);
+      s.service(resource);
+    };
+  }
+
+  template<class server_type>
+  void visit(server_type& s) {
+    visit_(s);
+  }
+
+  Handle(const Handle& r) : unknown_(r.unknown_), visit_(r.visit_) {
+
+  }
+
+  Handle(Handle&& r) noexcept : unknown_(r.unknown_), visit_(std::move(r.visit_)) {
+  }
+private:
+  void* unknown_;
+  std::function<void(Server& s)> visit_;
+};
+#endif//handle_hpp_20150209_1149
